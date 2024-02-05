108bc411b82c9fbc1c19922082cf16623d68efa3
rpy-aiwe <roman.pytkov@mail.ru>
2024-02-05 21:37:47 +0300

    Test backend published

--- scripts/client/setup_all.sh
+++ scripts/client/setup_all.sh
@@ -6,3 +6,6 @@
 cd $(git rev-parse --show-toplevel)/manifests
 
 kubectl apply -f traefik-config.yaml
+kubectl apply -f Deployments/backend-deployment.yaml
+kubectl apply -f Services/backend-service.yaml
+kubectl apply -f default-ingress.yaml
